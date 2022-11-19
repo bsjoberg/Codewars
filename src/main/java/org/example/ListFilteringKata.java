7277fe9eaefb581a168f80c434e272f30801cf7b
brian <brian.sjoberg@excella.com>
2022-11-19 09:18:57 -0500

    List Filtering kata complete solution with passing tests.

---
+++ src/main/java/org/example/ListFilteringKata.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListFilteringKata {
+    public static List<Object> filterList(final List<Object> list) {
+        List<Object> result = new ArrayList<>();
+
+        // Iterate through the list and only keep integers
+        for (Object o : list) {
+            if (o instanceof Number)
+                result.add(o);
+        }
+
+        // Return the List with the Strings filtered out
+        return result;
+    }
+}
---
+++ src/test/java/ListFilteringKataTest.java
@@ -0,0 +1,24 @@
+import org.example.ListFilteringKata;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ListFilteringKataTest {
+
+    @Test
+    public void example1() {
+        assertEquals(List.of(1, 2), ListFilteringKata.filterList(List.of(1, 2, "a", "b")), "For input: [1, 2, \"a\", \"b\"]");
+    }
+
+    @Test
+    public void example2() {
+        assertEquals(List.of(1, 0, 15), ListFilteringKata.filterList(List.of(1, "a", "b", 0, 15)), "For input: [1, \"a\", \"b\", 0, 15]");
+    }
+
+    @Test
+    public void example3() {
+        assertEquals(List.of(1, 2, 123), ListFilteringKata.filterList(List.of(1, 2, "aasf", "1", "123", 123)), "For input: [1, 2, \"aasf\", \"1\", \"123\", 123]");
+    }
+}
