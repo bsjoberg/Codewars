dca6e35f777e238c2b9d321670697525ba9af0fc
brian <brian.sjoberg@excella.com>
2022-11-20 21:03:25 -0500

    Tests are passing with solution.

--- src/main/java/ValidatePINCode.java
+++ src/main/java/ValidatePINCode.java
@@ -1,5 +1,12 @@
 public class ValidatePINCode {
     public static boolean validatePin(String s) {
-        return true;
+        String regex4Digits = "\\d{4}";
+        String regex6Digits = "\\d{6}";
+
+        if(s.matches(regex4Digits) || s.matches(regex6Digits)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
