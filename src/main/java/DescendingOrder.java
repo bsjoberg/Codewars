b0c4e8dd9ba3a973c3fd33b21254644e851e9f32
brian <brian.sjoberg@excella.com>
2022-11-20 20:43:34 -0500

    A much better way to solve the descending order kata.

--- src/main/java/DescendingOrder.java
+++ src/main/java/DescendingOrder.java
@@ -1,27 +1,12 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class DescendingOrder {
     private DescendingOrder() {}
 
-    public static int sortDesc(int num) {
-        StringBuilder integerString = new StringBuilder("");
-
-        // Parse into individual ints
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        do{
-            int temp = num % 10;
-            array.add(temp);
-            num /= 10;
-        } while  (num > 0);
-
-        // Sort the array
-        Collections.sort(array, Collections.reverseOrder());
-
-        for (int i = 0; i < array.size(); i++) {
-            integerString.append(array.get(i));
-        }
-
-        return Integer.parseInt(integerString.toString());
+    public static int sortDesc(final int num) {
+        String[] array = String.valueOf(num).split("");
+        Arrays.sort(array, Collections.reverseOrder());
+        return Integer.valueOf(String.join("", array));
     }
 }
