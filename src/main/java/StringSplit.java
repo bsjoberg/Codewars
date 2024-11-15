ce0ccba33dcab1e68f7918565e885d819e69ec86
Brian Sjoberg <brian_sjoberg@yahoo.com>
2024-11-15 04:56:29 -0500

    Just did some formatting.

--- src/main/java/StringSplit.java
+++ src/main/java/StringSplit.java
@@ -1,16 +1,15 @@
 public class StringSplit {
 
-    public static String[] solution(String input) {
-        //Write your code here
-        if(input.length() % 2 == 1) 
-            input += "_";
-        int arraySize = input.length() / 2;
-      
-      
-      String[] result = new String[arraySize];
-      for(int i = 0; i < arraySize; ++i)
-        result[i] = "" + input.charAt(i * 2) + input.charAt(1 + i * 2);
-      
-      return result;
-    }
+  public static String[] solution(String input) {
+    // Write your code here
+    if (input.length() % 2 == 1)
+      input += "_";
+    int arraySize = input.length() / 2;
+
+    String[] result = new String[arraySize];
+    for (int i = 0; i < arraySize; ++i)
+      result[i] = "" + input.charAt(i * 2) + input.charAt(1 + i * 2);
+
+    return result;
+  }
 }
