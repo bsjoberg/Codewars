150b07ce5574b6e0a7e8ab1f2127d6a0ae6826f2
Brian Sjoberg <brian_sjoberg@yahoo.com>
2024-12-26 11:46:39 -0500

    Updated to have CodeWars best practice solution.

--- src/main/java/SecureTester.java
+++ src/main/java/SecureTester.java
@@ -1,20 +1,6 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class SecureTester {
 
     public static boolean alphanumeric(String input) {
-        Pattern alphanumericPattern = Pattern.compile("[a-zA-Z0-9]");
-        Matcher countMatcher = alphanumericPattern.matcher(input);
-
-        if (input.length() == 0)
-            return false;
-        else {
-            int count = 0;
-            while (countMatcher.find())
-                count++;
-
-            return input.length() == count;
-        }
+        return input.matches("[A-Za-z0-9]+");
     }
 }
