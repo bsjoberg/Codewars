6991d2df81ea164c9cf6ff0b1bf84fbc8be5f8ef
Brian Sjoberg <brian_sjoberg@yahoo.com>
2025-03-04 08:22:48 -0500

    Removed too much.

--- src/main/java/org/GreedIsGood.java
+++ src/main/java/org/GreedIsGood.java
@@ -9,18 +9,26 @@ public class GreedIsGood {
     public static int greedy(int[] input) {
         int result = 0;
         int one_count = 0;
+        int four_count = 0;
 
         for (int i = 0; i < input.length; i++) {
             if (input[i] == 1) {
                 result += 100;
                 one_count++;
             }
+            else if (input[i] == 4) {
+                four_count++;
+            }
+
+            else if (input[i] == 5)
+                result += 50;
         }
 
         if (one_count / 3 >= 1)
-                result = result + 700;
+            result += 700;
+        else if (four_count / 3 >= 1)
+            result += 400;
 
         return result;
     }
-
 }
